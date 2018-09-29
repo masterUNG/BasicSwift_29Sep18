b5d64d3b4640ba89c429da1fbba9ee72636e67c9
masterUNG <phrombutr@gmail.com>
2018-09-29 14:34:19 +0700

    Optional

---
+++ Optional.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var numberInt: Int?
+
+print("numberInt ==> \(numberInt)")
+
+
+var numberString: String = "five"
+
+//let number2Int: Int = Int(numberString)!
+
+//print("number2Int ==> \(number2Int)")
+
+if var myNumber = Int(numberString) {
+    print("myNumber ==> \(myNumber)")
+}
+
+
+
+
+
