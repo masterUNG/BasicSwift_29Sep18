415605dcd132953d1bc1cfec9f0d7dc8e11ddd76
masterUNG <phrombutr@gmail.com>
2018-09-29 09:33:21 +0700

    variable and constant Success

---
+++ Basic_29Sep81.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+//การประกาศตัวแปร และ ค่าคงที่
+//Implicit
+var nameString = "Doramon"
+var number1Int = 123
+var number3Dou = 12.34
+var status1Bol = true
+
+var constNumberInt = 999
+
+number1Int = 555
+constNumberInt = 123
+
+
+//Explicit
+var surnameString: String = "Japane"
+var number2Int: Int = 321
+var number4Dou: Double = 12.34
+var status2Bol: Bool = false
+
+
+
+
+
+
